6f6595f14b71fafde1b0d428ce3fad3ce879a92a
Leo Feng <thcheetah777@gmail.com>
2024-05-11 08:57:57 -0500

    🔼 use vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,15 @@
 import { defineConfig } from "astro/config";
-
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 import svelte from "@astrojs/svelte";
 
+import vercel from "@astrojs/vercel/static";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://deltae.vercel.app",
-  integrations: [mdx(), sitemap(), tailwind(), svelte()]
+  integrations: [mdx(), sitemap(), tailwind(), svelte()],
+  output: "server",
+  adapter: vercel({ webAnalytics: true }),
 });
